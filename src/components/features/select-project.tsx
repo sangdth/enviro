1e4248425a9496ad5096c5aeb1b2806783b9e993
Sang Dang <sang@dang.to>
2024-04-20 09:00:27 +0300

    feat(ui): Add typography component and update button
    The changes include:
    
    1. Adding a new `typography` component to the `ui` module. This component provides
       a reusable way to render text with different size and variant options.
    2. Updating the `button` component to use the new `Slot` component from `@radix-ui/react-slot`.
    3. Adding a new `Size` type to the `types` module to represent the supported text sizes.
    4. Updating the `SelectProject` component to handle selecting a project directory
       and displaying the selected path.
    
    The main goal of these changes is to improve the UI consistency and provide a
    more robust set of UI components for the application.

--- src/components/features/select-project.tsx
+++ src/components/features/select-project.tsx
@@ -1,15 +1,59 @@
+import { readDir, BaseDirectory, DirEntry } from '@tauri-apps/plugin-fs';
 import { open } from '@tauri-apps/plugin-dialog';
-import { Button } from '@/components/ui';
+import { Button, Text } from '@/components/ui';
+import { useState } from 'react';
 
 // type SelectProjectProps = {}
 
 export const SelectProject = () => {
-  const handleClick = async () => {
-    const folder = await open({
+  const [selectedPath, setSelectedPath] = useState<string | null>(null);
+
+  const handleSelectFolder = async () => {
+    const dirPath = await open({
       directory: true,
     });
 
-    console.log('### folder: ', { folder });
+    console.log('### what: ', {
+      dirPath,
+      dir: BaseDirectory.Home,
+    });
+
+    if (!dirPath) {
+      return;
+    }
+
+    setSelectedPath(dirPath);
+
+    const entries = await readDir(dirPath);
+
+    for (const entry of entries) {
+      if (entry.isFile) {
+        console.log(`Found file: ${entry.name}`);
+        // } else if (entry.isDirectory && entry.children) {
+        // await readDirectory(entry.path); // Recursive call for subdirectories
+      }
+    }
   };
-  return <Button onClick={handleClick}>Select Project</Button>;
+  return (
+    <div className="flex flex-col">
+      <Button onClick={handleSelectFolder}>Select Project</Button>
+      {selectedPath && <Text variant="muted">{selectedPath}</Text>}
+    </div>
+  );
 };
+
+export async function processEntriesRecursive(
+  parent: string,
+  entries: DirEntry[]
+) {
+  for (const entry of entries) {
+    console.log(`Entry: ${entry.name}`);
+    if (entry.isDirectory) {
+      const dir = parent + entry.name;
+      processEntriesRecursive(
+        dir,
+        await readDir(dir, { baseDir: BaseDirectory.Home })
+      );
+    }
+  }
+}
